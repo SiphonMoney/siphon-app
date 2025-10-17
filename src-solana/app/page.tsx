f331d70de227e004aaf61f4d720e7cbef5dd0ad1
undefinedlab <thechakralabs@gmail.com>
2025-10-17 15:57:21 +0200

    Initial commit from Create Next App

---
+++ src-solana/app/page.tsx
@@ -0,0 +1,13 @@
+import Nav from "@/components/theme/Nav";
+import AppFooter from "@/components/theme/AppFooter";
+import WaterCanvas from "@/components/theme/WaterCanvas";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <AppFooter />
+      <WaterCanvas />
+    </div>
+  );
+}
