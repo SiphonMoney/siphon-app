9be8f4b929e487d0cc770625d280c5d7abf3f4b2
minrawsjar <swarnim.raj78@gmail.com>
2026-06-27 23:41:14 +0530

    changes for usdc price feed

--- src/lib/limitOrderTree.ts
+++ src/lib/limitOrderTree.ts
@@ -18,7 +18,7 @@ export const PYTH_PRICE_FEED_IDS: Record<string, string> = {
   SOL: "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d",
   ETH: "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
   BTC: "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
-  USDC: "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9c94a",
+  USDC: "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
 };
 
 export function createDefaultPriceConditionTree(): ConditionNode {
